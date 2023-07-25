f62fa800b9f98582d905e1e161559b2ec9236fc8
Nicolas <nop@outlook.com>
2023-07-25 23:44:31 +0200

    ajustments : w8ing time and variables

--- README.md
+++ README.md
@@ -5,13 +5,11 @@ it will download mysqld8.0.32.bat if you don't have it
 if you have it, change the script variable to match the location on your computer.
 
 Create a directory with database information if you don't have it
-else it considert that you already have a configured mysql server on your computer and abort !
+if you have it, change the script variable to match the location on your computer.
+No Connection attempt will be realised to modify the root password
 
-set the superuser with password as configured in the script variables
+----
 
-create a database in the mysql server
-
-
-If all these steps are already done when launching the script it will directly launch the server on configured port
+If all these steps are already done when launching the script it will directly launch the server on configured port (3001 by default)
 
 
--- sqlServer_8.0.32.bat
+++ sqlServer_8.0.32.bat
@@ -3,73 +3,65 @@ setlocal enabledelayedexpansion
 set current_path=%~dp0
 
 ::database configuration
-set DATABASE_HOST=127.0.0.1
-set DATABASE_PORT=3001
-::the database that will be automaticaly created
-set DATABASE_NAME=testDatabase
-::do not change user=root. if you want an other user create it manually after connecting with root.
-set DATABASE_USER=root 
-::strongly recommended to change this password for a stronger one !!!
-set DATABASE_PASSWORD=root
+set DATABASE_SERVER_PORT=3001
+:: Don't change USER If you want an other user create it manually after connecting with root.
+set DATABASE_SERVER_USER=root
+::strongly recommended to change this password for a stronger one !
+set DATABASE_SERVER_PASSWORD=root
 
 ::working dir
-set DATABASE_DIRECTORY_NAME=sqlServer
-set DATABASE_DATA_DIRECTORY_NAME=bdd_data
-set DATABASE_DIRECTORY_PATH=%current_path%%DATABASE_DIRECTORY_NAME%
-set DATABASE_DATA_DIRECTORY_PATH=%current_path%%DATABASE_DATA_DIRECTORY_NAME%
+set DATABASE_DIRECTORY_DESTINATION_PATH=%current_path%sqlServer
+set DATABASE_DATA_DIRECTORY_DESTINATION_PATH=%current_path%bdd_data
 
-::working files
+::working temporary file
 set zipName=MySqlServer.zip
 
 ::outside if else because batch is a shity thing
-set "password_prefix=A temporary password is generated for %DATABASE_USER%@localhost: "
+set "password_prefix=A temporary password is generated for %DATABASE_SERVER_USER%@localhost: "
 
-if %DATABASE_DIRECTORY_PATH% == %current_path% (
-  echo DATABASE_DIRECTORY_PATH cannot be the current path : %current_path%
+
+if %DATABASE_DIRECTORY_DESTINATION_PATH% == %current_path% (
+  echo DATABASE_DIRECTORY_DESTINATION_PATH cannot be the current path : %current_path%
   pause
   exit
 )
 
-if %DATABASE_DATA_DIRECTORY_PATH% == %current_path% (
-  echo DATABASE_DATA_DIRECTORY_PATH cannot be the current path : %current_path%
+if %DATABASE_DATA_DIRECTORY_DESTINATION_PATH% == %current_path% (
+  echo DATABASE_DATA_DIRECTORY_DESTINATION_PATH cannot be the current path : %current_path%
   pause
   exit
 )
 
-if %DATABASE_NAME% == 'database' (
-  echo DATABASE_NAME cannot be database
-)
 
-
-if exist "%DATABASE_DIRECTORY_PATH%" (
-  echo Directory %DATABASE_DIRECTORY_PATH% already exist. Skipping installation ...
+if exist "%DATABASE_DIRECTORY_DESTINATION_PATH%" (
+  echo Directory %DATABASE_DIRECTORY_DESTINATION_PATH% already exist. Skipping installation ...
 ) else (
-  echo download sql server archive from oracle website to %current_path%%zipName% ...
+  echo downloading sql server archive from oracle website : https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.32-winx64.zip to %current_path%%zipName% ...
   powershell -Command "& { Invoke-WebRequest 'https://dev.mysql.com/get/Downloads/MySQL-8.0/mysql-8.0.32-winx64.zip' -OutFile '%zipName%' }"
 
-  echo unzip %current_path%%zipName% to %DATABASE_DIRECTORY_PATH%...
-  powershell Expand-Archive -LiteralPath "%zipName%" -DestinationPath "%DATABASE_DIRECTORY_PATH%"
+  echo unzip %current_path%%zipName% to %DATABASE_DIRECTORY_DESTINATION_PATH%...
+  powershell Expand-Archive -LiteralPath "%zipName%" -DestinationPath "%DATABASE_DIRECTORY_DESTINATION_PATH%"
 
-  echo taking zip content to %DATABASE_DIRECTORY_PATH% ... 
-  xcopy "%DATABASE_DIRECTORY_PATH%\mysql-8.0.32-winx64\*" "%DATABASE_DIRECTORY_PATH%\" /E /H /C /Y
+  echo taking zip content to %DATABASE_DIRECTORY_DESTINATION_PATH% ... 
+  xcopy "%DATABASE_DIRECTORY_DESTINATION_PATH%\mysql-8.0.32-winx64\*" "%DATABASE_DIRECTORY_DESTINATION_PATH%\" /E /H /C /Y
 
   echo deleting :
   echo unzip file...
-  rd /S /Q "%DATABASE_DIRECTORY_PATH%\mysql-8.0.32-winx64"
+  rd /S /Q "%DATABASE_DIRECTORY_DESTINATION_PATH%\mysql-8.0.32-winx64"
   echo zip file ...
   del %zipName%
 )
 
 
-if exist "%DATABASE_DATA_DIRECTORY_PATH%" (
-  echo Directory %DATABASE_DATA_DIRECTORY_PATH% already exist. Skipping initialisation ...
+if exist "%DATABASE_DATA_DIRECTORY_DESTINATION_PATH%" (
+  echo Directory %DATABASE_DATA_DIRECTORY_DESTINATION_PATH% already exist. Skipping initialisation ...
 ) else (
   
-  echo initialize %DATABASE_DATA_DIRECTORY_PATH% directory with database data
-  powershell %DATABASE_DIRECTORY_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_PATH% --datadir=%DATABASE_DATA_DIRECTORY_PATH% --initialize --user=%DATABASE_USER%
+  echo initialize %DATABASE_DATA_DIRECTORY_DESTINATION_PATH% directory with database data
+  powershell %DATABASE_DIRECTORY_DESTINATION_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_DESTINATION_PATH% --datadir=%DATABASE_DATA_DIRECTORY_DESTINATION_PATH% --initialize --user=%DATABASE_SERVER_USER%
 
-  echo geting the errors file from %DATABASE_DATA_DIRECTORY_PATH% to %current_path% to prevent rights trouble
-  cd %DATABASE_DATA_DIRECTORY_PATH%
+  echo geting the errors file from %DATABASE_DATA_DIRECTORY_DESTINATION_PATH% to %current_path% to prevent rights trouble
+  cd %DATABASE_DATA_DIRECTORY_DESTINATION_PATH%
   xcopy "*.err" "%current_path%" /Y
   cd %current_path%
 
@@ -95,26 +87,25 @@ if exist "%DATABASE_DATA_DIRECTORY_PATH%" (
   echo deleting error files ...
   del *.err
 
-  echo Initialize MySql with super user %DATABASE_USER%
-  start %DATABASE_DIRECTORY_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_PATH% --datadir=%DATABASE_DATA_DIRECTORY_PATH% --initialize --user=%DATABASE_USER%
+  echo Initialize MySql with super user %DATABASE_SERVER_USER%
+  start %DATABASE_DIRECTORY_DESTINATION_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_DESTINATION_PATH% --datadir=%DATABASE_DATA_DIRECTORY_DESTINATION_PATH% --initialize --user=%DATABASE_SERVER_USER%
 
   echo start Mysql Server on new terminal
-  start %DATABASE_DIRECTORY_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_PATH% --datadir=%DATABASE_DATA_DIRECTORY_PATH% --console --port=%DATABASE_PORT%
+  start %DATABASE_DIRECTORY_DESTINATION_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_DESTINATION_PATH% --datadir=%DATABASE_DATA_DIRECTORY_DESTINATION_PATH% --console --port=%DATABASE_SERVER_PORT%
+  
+  echo waiting 30 secs to be sure database server is launch
+  timeout /t 30
 
-  echo setting configurated password : %DATABASE_PASSWORD% as new password for configurated user : %DATABASE_USER%
-  %DATABASE_DIRECTORY_PATH%/bin/mysql.exe -u%DATABASE_USER% -p!temporary_password! --port=%DATABASE_PORT% --execute="ALTER USER '%DATABASE_USER%'@'localhost' IDENTIFIED WITH mysql_native_password BY '%DATABASE_PASSWORD%';" --connect-expired-password
-
-  echo creating database %DATABASE_NAME%
-  %DATABASE_DIRECTORY_PATH%/bin/mysql.exe -u%DATABASE_USER% -p%DATABASE_PASSWORD% --port=%DATABASE_PORT% --execute="CREATE DATABASE IF NOT EXISTS %DATABASE_NAME%;"
+  echo setting configurated password : %DATABASE_SERVER_PASSWORD% as new password for configurated user : %DATABASE_SERVER_USER%
+  %DATABASE_DIRECTORY_DESTINATION_PATH%/bin/mysql.exe -u%DATABASE_SERVER_USER% -p!temporary_password! --port=%DATABASE_SERVER_PORT% --execute="ALTER USER '%DATABASE_SERVER_USER%'@'localhost' IDENTIFIED WITH mysql_native_password BY '%DATABASE_SERVER_PASSWORD%';" --connect-expired-password
 
   echo ----------------------------------------------------------------------------------------------------------------------------------------
-  echo MYSQL SERVER INITIALAZED FOR %DATABASE_USER% WITH PASSWORD : %DATABASE_PASSWORD%
-  echo DATABASE %DATABASE_NAME% HAVE BEEN CREATED ON IT
+  echo MYSQL SERVER INITIALAZED FOR %DATABASE_SERVER_USER% WITH PASSWORD : %DATABASE_SERVER_PASSWORD%
 
   pause
 )
 ::launch database server if needed
-%DATABASE_DIRECTORY_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_PATH% --datadir=%DATABASE_DATA_DIRECTORY_PATH% --console --port=%DATABASE_PORT% 
+%DATABASE_DIRECTORY_DESTINATION_PATH%/bin/mysqld.exe --basedir=%DATABASE_DIRECTORY_DESTINATION_PATH% --datadir=%DATABASE_DATA_DIRECTORY_DESTINATION_PATH% --console --port=%DATABASE_SERVER_PORT% 
 
 endlocal
 ::Everything done ...
